9102c5cbe0845892ee3595812fbd3cc7bd7d7928
Caleb Rosebaugh <rosebaugh@ksu.edu>
2022-03-26 16:00:13 -0500

    Arm with rotations

---
+++ Sprites/Items/StartingGun.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeGame.Sprites.Items
+{
+    public class StartingGun : Item
+    {
+        public StartingGun()
+        {
+            this.pixelWidth = 64;
+            this.pixelHeight = 32; 
+        }
+        public override void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("ArmPistol");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            //https://stackoverflow.com/questions/7339574/xna-rotating-a-sprite-to-face-the-cursor-exactly
+            MouseState ms = Mouse.GetState(); ;
+            rotationdir = (float)Math.Atan2((ms.Y - Position.Y), (ms.X - Position.X));
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            var source = new Rectangle(0, 0, this.pixelWidth, this.pixelHeight);
+            spriteBatch.Draw(texture, Position, source, Color.White, rotation, new Vector2(this.pixelHeight / 2, this.pixelHeight / 2), 1, SpriteEffects.None, 0);
+        }
+    }
+}
