01c9a3b73764b79bb5c6679f430f32462206303c
Caleb Niehues <chn@ksu.edu>
2022-03-26 10:07:54 -0500

    collisions added

---
+++ Collisions/IBounding.cs
@@ -0,0 +1,15 @@
+﻿namespace TimeGame.Collisions
+{
+    public interface IBounding
+    {
+        /// <summary>
+        /// Detects a collision between two Bounding items
+        /// </summary>
+        /// <param name="other">The other bounding item</param>
+        /// <returns>true = collision, false = no collison</returns>
+        public bool CollidesWith(IBounding other)
+        {
+            return CollisionHelper.Collides(this, other);
+        }
+    }
+}
