cb176f8acdc8e5fb37d356952ed9ce375bdd7fad
Caleb Rosebaugh <rosebaugh@ksu.edu>
2022-04-13 16:57:31 -0500

    Added Crate and Cameras

---
+++ Views/ICamera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TimeGame.Views
+{
+    /// <summary>
+    /// An interface defining a camera
+    /// </summary>
+    public interface ICamera
+    {
+        /// <summary>
+        /// The view matrix
+        /// </summary>
+        Matrix View { get; }
+
+        /// <summary>
+        /// The projection matrix
+        /// </summary>
+        Matrix Projection { get; }
+    }
+}
